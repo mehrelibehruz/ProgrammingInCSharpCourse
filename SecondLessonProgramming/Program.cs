2d52d69bcb9cb8225ee2f7f9a89275d0f452e7fd
Behruz Mehreli <mehrelibehruz@gmail.com>
2023-05-20 13:06:36 +0400

    Project initially added

---
+++ SecondLessonProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SecondLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte var1;
+            byte var2;
+
+            var2 = byte.Parse(Console.ReadLine());
+            var1 = 45;
+
+            int sum = var1 + var2;
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
